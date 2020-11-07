0ad161c9deb8b0ff012243a6335addb9efa895ef
Patti85 <pbeier24@googlemail.com>
2020-11-07 21:37:22 +0100

    first

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+namespace Pomodoro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Task.Delay(10000).ContinueWith(_ =>
+            {
+                showNotification();
+            });
+            
+
+
+        }
+
+        public void showNotification()
+        {
+            var message = "Pomodoro Pause!";
+            var xml = $"<?xml version=\"1.0\"?><toast><visual><binding template=\"ToastText01\"><text id=\"1\">{message}</text></binding></visual></toast>";
+            var toastXml = new XmlDocument();
+            toastXml.LoadXml(xml);
+            var toast = new ToastNotification(toastXml);
+            ToastNotificationManager.CreateToastNotifier("Pomodoro Pause!").Show(toast);
+        }
+    }
+}
